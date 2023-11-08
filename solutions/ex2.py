1429920a131b05d157d5993d7c147f2da8bbd1f7
elanadanseglio <148129709+elanadanseglio@users.noreply.github.com>
2023-11-08 11:01:58 -0500

    Create ex2.py

---
+++ solutions/ex2.py
@@ -0,0 +1,13 @@
+# Elana Danseglio
+# Kevin Ramos
+
+def main():
+    width = input("Enter width: ")
+    height = input("Enter height: ")
+    length = input("Enter length: ")
+    
+    volume = float(width) * float(height) * float(length)
+    print("Volume is: " , str(volume))    
+    
+if __name__ == '__main__':
+    main()
